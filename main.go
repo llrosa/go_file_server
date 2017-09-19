bb1edb3c3e581842984b30b3857c02e018a8717c
Lucas Rosa <lucas.rosa@hpe.com>
2017-09-19 10:22:37 -0300

    adding files

---
+++ main.go
@@ -0,0 +1,34 @@
+//(C) Copyright 2016-2017 Hewlett Packard Enterprise Development LP
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"flag"
+)
+
+
+func main() {
+	port := flag.Int("port", 8080, "File server port")
+	volume := flag.String("volume", "/tmp/", "File server port")
+
+	flag.Parse()
+
+	fileServerhostname := fmt.Sprintf(":%d", *port)
+
+	fmt.Printf("Starting File Server at %s with volume %s\n", fileServerhostname, *volume)
+	createFolderIfNeeded(*volume)
+	panic(http.ListenAndServe(fileServerhostname, http.FileServer(http.Dir(*volume))))
+}
+
+func createFolderIfNeeded(path string) {
+	//If path does not exist
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		fmt.Printf("Creating Folder %s\n", path)
+		err := os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
